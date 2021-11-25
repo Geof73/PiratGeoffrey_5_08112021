ce4d5b1530f6418443ffbd7ee4bc9408d405caeb
Geof73 <gpiratmail@gmail.com>
2021-11-25 22:09:28 +0100

    test product.js

--- front/js/product.js
+++ front/js/product.js
@@ -2,20 +2,33 @@ var searchUrl = new URLSearchParams(window.location.search)
 var getId = searchUrl.get("id")
 console.log(getId);
 
-async function test() {
+(async function () {
     const response = await fetch(`http://localhost:3000/api/products/${getId}`)
     const resp = await response.json()
 
     let html = ''
     for (const product of resp)
-    html +=
-        `<div class="item__img">
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-            </div>`
-    
-        document.getElementsByClassName(item__img).innerHTML = html
+        html +=
+            `<div class="item__img">
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                </div>`
+        
+            document.getElementsByClassName(item__img).innerHTML = html
 
-}
+})() 
+
+/*const response = fetch(`http://localhost:3000/api/products/${getId}`)
+    resp = console.log()
+
+    let html = ''
+    for (const product of html)
+        html +=
+            `<div class="item__img">
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                </div>`
+        
+            document.getElementsByClassName(item__img).innerHTML = html
+
 
 /*(async function () {
     const response = await fetch(`http://localhost:3000/api/products/${getId}`)
