a2648d33c34be8af2863ffe1b11fa7a1d90c2127
Geof73 <gpiratmail@gmail.com>
2021-11-25 19:01:11 +0100

    ajout product.js

--- front/js/product.js
+++ front/js/product.js
@@ -2,15 +2,30 @@ var searchUrl = new URLSearchParams(window.location.search)
 var getId = searchUrl.get("id")
 console.log(getId);
 
-(async function () {
+async function test() {
+    const response = await fetch(`http://localhost:3000/api/products/${getId}`)
+    const resp = await response.json()
+
+    let html = ''
+    for (const product of resp)
+    html +=
+        `<div class="item__img">
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+            </div>`
+    
+        document.getElementsByClassName(item__img).innerHTML = html
+
+}
+
+/*(async function () {
     const response = await fetch(`http://localhost:3000/api/products/${getId}`)
     const articles = await response.json()
-    
+
     let html = ''
     for (const product of articles)
         html =+
             `<div class="item__img">
             <img src="${product.imageUrl}" alt="${product.altTxt}">
             </div>`
-    document.querySelectorAll(item>div).innerHTML = html;
-})()
+    document.querySelectorAll("item > div").innerHTML = html;
+})()*/
