56d739c1871d1a1e0e4a18143e052cb9816b1f60
Geof73 <gpiratmail@gmail.com>
2021-12-16 23:44:58 +0100

    Avancement

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,52 +4,80 @@ window.addEventListener('DOMContentLoaded', async function () {
   let recupererDonnee = JSON.parse(localStorage.getItem("donneeUtilisateur"));
   console.log(recupererDonnee)
 
-  const response = await fetch(`http://localhost:3000/api/products/${recupererDonnee[0]}`)
-  const product = await response.json()
- 
-  const getDivId = document.getElementById("cart__items");
+  /* Création d'un élément div pour insérer par la suite l'image du produit.
   const createDiv = document.createElement("div");
-  createDiv.setAttribute('id', 'cart__item__img')
-  getDivId.appendChild(createDiv)
-  console.log(createDiv)
+  createDiv.setAttribute('class', 'cart__item__img');*/
+  let test = recupererDonnee[0].getId
 
-  const getIdImg = document.getElementById("cart__item__img");
-  const getImage = document.createElement("img");
-  getImage.src = product.imageUrl;
-  getImage.alt = product.altTxt;
 
-  getIdImg.appendChild(getImage)
-  
-  let html = ''
-  for (const element of recupererDonnee)
-    html +=
-      `<!--  <article class="cart__item" data-id="${element.getId}" data-color="${element.color}">
-            <div class="cart__item__img">
-              <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+  if (recupererDonnee == null) {
+    document.getElementById("cart__items").innerHTML = "Votre panier est vide"
+  }
+  else {
+
+    const response = await fetch("http://localhost:3000/api/products/")
+    const product = await response.json()
+    // Création d'un élément img puis paramétrer l'image avec les données de l'API.
+    const getImage = document.createElement("img");
+    getImage.src = product.imageUrl;
+    getImage.alt = product.altTxt;
+    console.log(getImage)
+
+    // Insertion de l'élément div créé précédemment à la section "cart__items" en tant qu'enfant.
+    const getItemId = document.getElementById("cart__items");
+    getItemId.appendChild(getImage)
+    console.log(getItemId)
+
+    /* Insertion de l'image en tant qu'enfant à la div créé.
+    const getItemImg = document.getElementsByClassName("cart__item__img");
+    getItemImg.appendChild(getImage);*/
+
+    document.getElementsByClassName("cart__item__content__description")[0] = product.name
+
+    // Modification de la quantité
+    let bouttonSuppr = document.getElementsByClassName("deleteItem")
+
+    bouttonSuppr.addEventListener('click', function () {
+      const itemQuantity = document.getElementsByClassName('itemQuantity').value
+      let calculQuantity = itemQuantity - recupererDonnee.quantity
+
+    })
+
+    let html = ''
+    for (const element of recupererDonnee)
+      html +=
+        `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+        <div class="cart__item__img">
+          <img src="${product.image}" alt="Photographie d'un canapé">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${product.name}</h2>
+            <p>${product.color}</p>
+            <p>${product.price}</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté : </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
             </div>
-            <div class="cart__item__content">
-              <div class="cart__item__content__description">
-                <h2>Nom du produit</h2>
-                <p>Vert</p>
-                <p>42,00 €</p>
-              </div>
-              <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                  <p>Qté : </p>
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                </div>
-                <div class="cart__item__content__settings__delete">
-                  <p class="deleteItem">Supprimer</p>
-                </div>
-              </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
             </div>
-          </article> -->`
+          </div>
+        </div>
+      </article>`
 
-  document.getElementById('cart__items').innerHTML = html
+    document.getElementById('cart__items').innerHTML = html
 
-  let calculationPrix = recupererDonnee[1] * product.price
 
-  document.getElementById("totalQuantity").innerHTML = recupererDonnee[1]
-  document.getElementById("totalPrice").innerHTML = calculationPrix
+    // Initialisation d'une variable qui permettra le calcul du prix total par rapport à la quantité et au prix du produit.
+    let calculationPrix = recupererDonnee[0].quantity * product.price
+
+    // Récupération depuis le localstorage de la quantité sélectionnée par l'utilisateur pour permettre le calcul.
+    document.getElementById("totalQuantity").innerHTML = recupererDonnee[0].quantity
+    document.getElementById("totalPrice").innerHTML = calculationPrix
+
+  }
 })
 
--- front/js/product.js
+++ front/js/product.js
@@ -36,34 +36,124 @@ window.addEventListener('DOMContentLoaded', async function () {
         selectColors.appendChild(option);
     }
 
-   // Récupération du DOM du bouton "Ajouter au panier" qui permettra la sauvegarde des paramètres choisie de l'usager dans le localstorage.
+    // Récupération du DOM du bouton "Ajouter au panier" qui permettra la sauvegarde des paramètres choisie de l'usager dans le localstorage.
     const button = document.getElementById('addToCart')
 
-     // Création d'une fonction qui au clic de l'utilisateur sur le bouton "Ajouter au panier", récolte les informations pour les stocker dans le localstorage, qui seront utilisées dans le panier.
+    // Création d'une fonction qui au clic de l'utilisateur sur le bouton "Ajouter au panier", récolte les informations pour les stocker dans le localstorage, qui seront utilisées dans le panier.
     button.addEventListener('click', function () {
 
         const color = document.getElementById("colors").value
         const quantity = document.getElementById("quantity").value
 
         // Stockage des données choisie de l'usager dans un tableau.
-        let stock = [getId, quantity, color];
-        console.log(stock)
+
+        class myStock {
+            constructor(getId, quantity, color) {
+                this.getId = getId;
+                this.quantity = quantity;
+                this.color = color;
+            }
+        }
+
+        // Création d'une variable pour stocker les informations
+        let stockProduit = []
+
 
         // Création d'une variable qui récupérera les données convertie en JSON si jamais l'utilisateur choisi de modifier la quantité ou la couleur d'un canapé.
         let recupererDonnee = JSON.parse(localStorage.getItem("donneeUtilisateur"));
 
-        // Boucle permettant de soit stocker l'ensemble des données dans le localstorage, ou de récupérer uniquement la quantité et la couleur si l'utilisateur à déjà effectuer un ajout au panier du produit. 
-        if (recupererDonnee == null) {
-            const stockerDonnee = localStorage.setItem("donneeUtilisateur", JSON.stringify(stock));
-            window.confirm('Votre sélection a bien été ajoutée au panier.');
+        if (recupererDonnee) {
+            recupererDonnee = JSON.parse(localStorage.getItem("donneeUtilisateur"))
+            let getDonnee = ''
+            for (const element of recupererDonnee)
+                getDonnee += `{${element.getId} ${element.quantity} ${element.color}}`;
+            console.log(getDonnee)
+            stockProduit.push(getDonnee)
+            localStorage.setItem("donneeUtilisateur", JSON.stringify(stockProduit));
+            window.confirm('Modification du panier')
         }
         else {
-            recupererDonnee.push(color, quantity)
-            const stockerDonnee = localStorage.setItem("donneeUtilisateur", JSON.stringify(stock));
-            window.confirm('Votre panier a été mis à jour !');
+            let ajoutProduit = new myStock(getId, quantity, color)
+            stockProduit.push(ajoutProduit);
+            localStorage.setItem("donneeUtilisateur", JSON.stringify(stockProduit));
+            console.log(stockProduit)
+
+            window.confirm('Ajout au panier')
         }
 
 
+
+        /*if (recupererDonnee) {
+            let test = ''
+            test = recupererDonnee
+            for (const element of recupererDonnee)
+                test += `{${element.getId} ${element.quantity} ${element.color}}`;
+            console.log(test)
+
+            recupererDonnee.pop(test)
+            let ajoutProduit = new myStock(getId, quantity, color)
+            recupererDonnee.push(ajoutProduit)
+
+            localStorage.setItem("donneeUtilisateur", JSON.stringify(recupererDonnee));
+            console.log(recupererDonnee)
+            window.confirm('Modifications apportées au panier')
+        }
+
+        // Si aucun produit où de même nom se trouvant dans le panier.
+        else {
+           
+            let ajoutProduit = new myStock(getId, quantity, color)
+            stockProduit.push(ajoutProduit)
+            localStorage.setItem("donneeUtilisateur", JSON.stringify(stockProduit));
+            console.log(recupererDonnee)
+            window.confirm('Produit ajouté')
+        }*/
     })
 })
 
+
+/*
+const cart = [
+    ['567Nkb88U', 'Pink', 4],
+    ['567Nkb88U', 'Orange', 2]
+]
+
+const cart = [
+    {
+        id: '567Nkb88U',
+        color: 'Pink',
+        qty: 5
+    },
+    {
+        id: '567Nkb88U',
+        color: 'Orange',
+        qty: 2
+    },
+    {
+        id: '867HJHB',
+        color: 'Blue',
+        qty: 1
+    }
+]
+
+
+const cart = {
+    '567Nkb88U_Pink': 4,
+    '567Nkb88U_Orange': 2
+}
+
+class Kanap {
+    constructor( id, color ) {
+        this.id = id
+        this.color = color
+    }
+
+    test() {
+        alert('Hello')
+    }
+
+}
+
+
+
+Object.setPrototypeOf(kanap, kanapProto)*/
