15b7502c8aac4b9b103b2ac4b1fab9f459ae8226
Geof73 <gpiratmail@gmail.com>
2021-12-24 10:31:38 +0100

    Etape 7 ajouté

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,23 +1,23 @@
 // Création d'une fonction qui une fois la page chargée, declenche son contenu.
 window.addEventListener('DOMContentLoaded', async function () {
 
-  let recupererDonnee = JSON.parse(localStorage.getItem("donneeUtilisateur"));
-  console.log(recupererDonnee)
-
-  /* Création d'un élément div pour insérer par la suite l'image du produit.
-  const createDiv = document.createElement("div");
-  createDiv.setAttribute('class', 'cart__item__img');*/
-  let test = recupererDonnee[0].getId
-
+  // Récupération des données du localstorage enregistrées de la page "product.js".
+  let recupererDonnee = JSON.parse(localStorage.getItem("Data"));
+  console.log(recupererDonnee == null)
 
   if (recupererDonnee == null) {
     document.getElementById("cart__items").innerHTML = "Votre panier est vide"
   }
   else {
 
-    const response = await fetch("http://localhost:3000/api/products/")
+    // Récupération de l'API du serveur obtenir des éléments de description des canapés.
+    const response = await fetch("http://localhost:3000/api/products")
     const product = await response.json()
-    // Création d'un élément img puis paramétrer l'image avec les données de l'API.
+
+
+    // Insertion des éléments récupérés dans le DOM de la page "Panier".
+
+    // Insertion de l'image du canapé.
     const getImage = document.createElement("img");
     getImage.src = product.imageUrl;
     getImage.alt = product.altTxt;
@@ -28,27 +28,24 @@ window.addEventListener('DOMContentLoaded', async function () {
     getItemId.appendChild(getImage)
     console.log(getItemId)
 
-    /* Insertion de l'image en tant qu'enfant à la div créé.
-    const getItemImg = document.getElementsByClassName("cart__item__img");
-    getItemImg.appendChild(getImage);*/
+    // Création d'un élément div pour insérer par la suite l'image du produit.
+    const createDiv = document.createElement("div");
+    createDiv.setAttribute('class', 'cart__item__img');
 
+    // Insertion de l'image en tant qu'enfant à la div créé.
+    const getItemImg = document.getElementsByClassName("cart__item__img");
+    getItemImg.appendChild(getImage);
+
+    // Insertion du nom du produit
     document.getElementsByClassName("cart__item__content__description")[0] = product.name
 
-    // Modification de la quantité
-    let bouttonSuppr = document.getElementsByClassName("deleteItem")
-
-    bouttonSuppr.addEventListener('click', function () {
-      const itemQuantity = document.getElementsByClassName('itemQuantity').value
-      let calculQuantity = itemQuantity - recupererDonnee.quantity
-
-    })
-
+    // Création d'une boucle pour afficher sur le DOM les informations des canapés récupérées depuis l'API.
     let html = ''
     for (const element of recupererDonnee)
       html +=
-        `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+        `<article class="cart__item" data-id="${recupererDonnee.getId}" data-color="${recupererDonnee.color}">
         <div class="cart__item__img">
-          <img src="${product.image}" alt="Photographie d'un canapé">
+          <img src="${product.image}" alt="${product.altTxt}>
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__description">
@@ -58,7 +55,7 @@ window.addEventListener('DOMContentLoaded', async function () {
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
-              <p>Qté : </p>
+              <p>${recupererDonnee.quantity}</p>
               <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
             </div>
             <div class="cart__item__content__settings__delete">
@@ -72,12 +69,39 @@ window.addEventListener('DOMContentLoaded', async function () {
 
 
     // Initialisation d'une variable qui permettra le calcul du prix total par rapport à la quantité et au prix du produit.
-    let calculationPrix = recupererDonnee[0].quantity * product.price
+    let calculationPrix = recupererDonnee.quantity * product.price
 
     // Récupération depuis le localstorage de la quantité sélectionnée par l'utilisateur pour permettre le calcul.
     document.getElementById("totalQuantity").innerHTML = recupererDonnee[0].quantity
     document.getElementById("totalPrice").innerHTML = calculationPrix
-
   }
-})
+
+  // Modifier ou supprimer la quantité du produit
+
+  // Récupation du boutton "Supprimer" pour moidifier la quantité des produits.
+  let bouttonSuppr = document.getElementsByClassName("deleteItem")
+
+  // Création d'un évenement lié au clic du boutton "supprimer"
+  bouttonSuppr.addEventListener('change', (event) => {
+    const itemQuantity = document.getElementsByClassName('itemQuantity').value
+    
+  });
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,8 @@
+
+
 // Création d'une variable permettant d'extraire l'url de la page, et d'ensuite récupérer son ID pour l'insérer dans une variable.
-const searchUrl = new URLSearchParams(window.location.search)
-const getId = searchUrl.get("id")
-console.log(getId);
+const searchUrl = new URLSearchParams(window.location.search);
+const getId = searchUrl.get("id");
 
 // Déclenchement d'une fonction au chargement du DOM
 window.addEventListener('DOMContentLoaded', async function () {
@@ -18,7 +19,6 @@ window.addEventListener('DOMContentLoaded', async function () {
     // Récupération du contenant "item__img" du DOM puis d'insérer l'image en tant qu'enfant.
     const getDiv = document.getElementsByClassName("item__img")[0];
     getDiv.appendChild(getImage)
-    console.log(getDiv)
 
     // Récupérer des données depuis l'API pour l'insérer au DOM.
     document.getElementById('title').textContent = product.name
@@ -45,115 +45,18 @@ window.addEventListener('DOMContentLoaded', async function () {
         const color = document.getElementById("colors").value
         const quantity = document.getElementById("quantity").value
 
-        // Stockage des données choisie de l'usager dans un tableau.
+        let produit = {getId, quantity, color}
 
-        class myStock {
-            constructor(getId, quantity, color) {
-                this.getId = getId;
-                this.quantity = quantity;
-                this.color = color;
-            }
-        }
+        const myCart = new Panier()
 
-        // Création d'une variable pour stocker les informations
-        let stockProduit = []
-
-
-        // Création d'une variable qui récupérera les données convertie en JSON si jamais l'utilisateur choisi de modifier la quantité ou la couleur d'un canapé.
-        let recupererDonnee = JSON.parse(localStorage.getItem("donneeUtilisateur"));
-
-        if (recupererDonnee) {
-            recupererDonnee = JSON.parse(localStorage.getItem("donneeUtilisateur"))
-            let getDonnee = ''
-            for (const element of recupererDonnee)
-                getDonnee += `{${element.getId} ${element.quantity} ${element.color}}`;
-            console.log(getDonnee)
-            stockProduit.push(getDonnee)
-            localStorage.setItem("donneeUtilisateur", JSON.stringify(stockProduit));
-            window.confirm('Modification du panier')
-        }
-        else {
-            let ajoutProduit = new myStock(getId, quantity, color)
-            stockProduit.push(ajoutProduit);
-            localStorage.setItem("donneeUtilisateur", JSON.stringify(stockProduit));
-            console.log(stockProduit)
-
-            window.confirm('Ajout au panier')
-        }
-
-
-
-        /*if (recupererDonnee) {
-            let test = ''
-            test = recupererDonnee
-            for (const element of recupererDonnee)
-                test += `{${element.getId} ${element.quantity} ${element.color}}`;
-            console.log(test)
-
-            recupererDonnee.pop(test)
-            let ajoutProduit = new myStock(getId, quantity, color)
-            recupererDonnee.push(ajoutProduit)
-
-            localStorage.setItem("donneeUtilisateur", JSON.stringify(recupererDonnee));
-            console.log(recupererDonnee)
-            window.confirm('Modifications apportées au panier')
-        }
-
-        // Si aucun produit où de même nom se trouvant dans le panier.
-        else {
-           
-            let ajoutProduit = new myStock(getId, quantity, color)
-            stockProduit.push(ajoutProduit)
-            localStorage.setItem("donneeUtilisateur", JSON.stringify(stockProduit));
-            console.log(recupererDonnee)
-            window.confirm('Produit ajouté')
-        }*/
+        myCart.add(produit)
+        
+        myCart.save()
+        
     })
 })
 
 
-/*
-const cart = [
-    ['567Nkb88U', 'Pink', 4],
-    ['567Nkb88U', 'Orange', 2]
-]
-
-const cart = [
-    {
-        id: '567Nkb88U',
-        color: 'Pink',
-        qty: 5
-    },
-    {
-        id: '567Nkb88U',
-        color: 'Orange',
-        qty: 2
-    },
-    {
-        id: '867HJHB',
-        color: 'Blue',
-        qty: 1
-    }
-]
-
-
-const cart = {
-    '567Nkb88U_Pink': 4,
-    '567Nkb88U_Orange': 2
-}
-
-class Kanap {
-    constructor( id, color ) {
-        this.id = id
-        this.color = color
-    }
-
-    test() {
-        alert('Hello')
-    }
-
-}
 
 
 
-Object.setPrototypeOf(kanap, kanapProto)*/
