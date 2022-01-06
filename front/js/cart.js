1a695dc98aac16bba06cf63264734253b0ca6ae3
Geof73 <gpiratmail@gmail.com>
2022-01-07 00:13:29 +0100

    ajout modification produit, reste la suppression a faire

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,25 +3,6 @@ let myCart = new Panier()
 // Création d'une fonction qui une fois la page chargée, declenche son contenu.
 window.addEventListener('DOMContentLoaded', async function () {
 
-  // Validation du formulaire en l'envoyant au serveur via la méthode POST.
-  const contact = JSON.parse(localStorage.getItem("Contact"));
-  const products = JSON.parse(localStorage.getItem("Data"));
-
-  const command = { contact, products }
-  console.log(command)
-
-  const postFetch = await fetch("http://localhost:3000/api/products/order", {
-    method: "POST",
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(command)
-  });
-  console.log(postFetch)
-  const response = await postFetch.json();
-  console.log(response);
-
   // Récupération des données du localstorage enregistrées depuis la page "product.js".
   if (myCart.getNumberProduct() === 0) {
     document.getElementById("cart__items").innerHTML = "Votre panier est vide"
@@ -68,21 +49,35 @@ window.addEventListener('DOMContentLoaded', async function () {
 // Changement de la quantité ou suppression d'un produit si la quantité est à 0.
 let changerQuantite = document.getElementById("cart__items").addEventListener('click', function (event) {
   console.log(event)
-
-  let element = document.getElementById('cart__items')
-
-  let getP = event.composedPath()[4]
-
-  /*let test = event.target.closest('#cart__items').dataset.id
-  console.log(test)*/
-
-  getP.addEventListener('change', (event) => {
-    console.log(event)
-    let elementId = getP.dataset.id
-    let elementColor = getP.dataset.color
-    let stockElement = { elementId, elementColor }
-    localStorage.setItem("ID", JSON.stringify(stockElement));
-    myCart.changeQuantity()
+  const row = event.target.closest('article.cart__item')
+  row.dataset.id
+  row.dataset.color
+  row.addEventListener('change', (event) => {
+    if (event.target.matches('p.deleteItem')) {
+      let eventQuantity = parseFloat(event.target.value);
+      let foundId = myCart.panier.find(e => e.getId == row.dataset.id);
+      let foundColor = myCart.panier.find(e => e.color == row.dataset.color);
+      let product = foundId = foundColor
+      product.quantity = eventQuantity
+      myCart.save();
+      if (eventQuantity < 0) {
+        myCart.panier.filter(p => p.getId == product);
+        myCart.save()
+        location.reload()
+      }
+    } else if (event.target.matches('input.itemQuantity')) {
+      let eventQuantity = parseFloat(event.target.value);
+      let foundId = myCart.panier.find(e => e.getId == row.dataset.id);
+      let foundColor = myCart.panier.find(e => e.color == row.dataset.color);
+      let product = foundId = foundColor
+      product.quantity = eventQuantity
+      myCart.save();
+      if (eventQuantity < 0) {
+        myCart.panier.filter(p => p.getId == product);
+        myCart.save()
+        location.reload()
+      }
+    }
   })
 })
 
@@ -90,66 +85,87 @@ let changerQuantite = document.getElementById("cart__items").addEventListener('c
 let getForm = document.querySelector(".cart__order__form").addEventListener('change', (event) => {
   console.log(event)
 
+  //
+  let getJsonData = JSON.parse(localStorage.getItem("Data"));
+  let getJsonContact = JSON.parse(localStorage.getItem("Contact"));
+
   let firstName = document.getElementById('firstName').value
+  document.getElementById("firstNameErrorMsg").innerHTML = ""
   let lastName = document.getElementById('lastName').value
+  document.getElementById("lastNameErrorMsg").innerHTML = ""
   let address = document.getElementById('address').value
+  document.getElementById("addressErrorMsg").innerHTML = ""
   let city = document.getElementById('city').value
+  document.getElementById("cityErrorMsg").innerHTML = ""
   let email = document.getElementById('email').value
+  document.getElementById("emailErrorMsg").innerHTML = ""
+
   let contact = { firstName, lastName, address, city, email }
-  let stockDataForm = []
-  if (contact != null) {
-    stockDataForm.pop()
-    stockDataForm.push(contact)
-    localStorage.setItem("Contact", JSON.stringify(contact))
-
-  }
-  else {
-    stockDataForm.push(contact)
-    localStorage.setItem("Contact", JSON.stringify(contact))
-  }
-
-
+  localStorage.setItem("Contact", JSON.stringify(contact));
 
   // Création de conditions pour que l'usager puisse valider le formulaire.
-  let clicOrder = document.getElementById('order').addEventListener('click', function (event) {
+  if (getJsonData != undefined) {
+    document.getElementById('order').addEventListener('click', function (event) {
+      const contact = { getJsonData, getJsonContact };
+      post();
 
-    let getJsonData = localStorage.getItem("Data");
-    let addPatternFirstName = document.getElementById("firstName").setAttribute("pattern","[a-z]{3,}")
-    if (firstName != undefined) {
-      let firstNameErrorMsg = document.getElementById("firstNameErrorMsg").innerHTML = "Veuillez entrer votre prénom."
-    }
-    let addPatternLastName = document.getElementById("lastName").setAttribute("pattern","[a-z]{3,}")
-    if (lastName != undefined) {
-      let lastNameErrorMsg = document.getElementById("lastNameErrorMsg").innerHTML = "Veuillez entrer votre nom."
-    }
-    let addPatternAddress = document.getElementById("address").setAttribute("pattern","[a-zA-Z0-9]{6}")
-    if (address != undefined) {
-      let addressErrorMsg = document.getElementById("addressErrorMsg").innerHTML = "Veuillez entrer votre adresse."
-    }
-    let addPatternCity = document.getElementById("city").setAttribute("pattern","[a-z]{3,}")
-    if (city != undefined) {
-      let cityErrorMsg = document.getElementById("cityErrorMsg").innerHTML = "Veuillez entrer votre ville."
-    }
-    if (email != undefined) {
-      let emailErrorMsg = document.getElementById("emailErrorMsg").innerHTML = "Veuillez entrer votre email."
-    }
+      if (firstName.getJsonContact == undefined) {
+        document.getElementById("firstNameErrorMsg").innerHTML = "Veuillez entrer votre nom, uniquement composé de lettres."
+      }
+      if (lastName.getJsonContact == undefined) {
+        document.getElementById("lastNameErrorMsg").innerHTML = "Veuillez entrer votre nom, uniquement composé de lettres."
+      }
+      if (address.getJsonContact == undefined) {
+        document.getElementById("addressErrorMsg").innerHTML = "Veuillez entrer votre adresse."
+      }
+      if (city.getJsonContact == undefined) {
+        document.getElementById("cityErrorMsg").innerHTML = "Veuillez entrer votre ville, uniquement composée de lettres.."
+      }
+      if (email.getJsonContact == undefined) {
+        document.getElementById("emailErrorMsg").innerHTML = "Veuillez entrer une adresse email valide."
+      }
 
-    let getOrderId = document.getElementById("orderId")
-    getJsonData = localStorage.getItem("Data");
-    let getJsonForm = localStorage.getItem("Form");
-    const contact = { getJsonData, getJsonForm }
-  });
-
-})
-/*  if (response) {
-    alert(`Votre commande numéro ${response.orderId} à bien été passée.`)
-    localStorage.setItem()
-    localStorage.setItem()
-    window.location = `C:\Users\gpira\Desktop\Formation\P5_pirat_geoffrey\front\html`
-  } else {
-    alert(`Erreur de commande`)
+    })
   }
-  alert(`Votre commande numéro ${response.orderId} à bien été passée.`)
-})*/
+  else {
+    window.alert("Veuillez d'abord ajouter des éléments à votre panier")
+  }
+
+  // Validation du formulaire en l'envoyant au serveur via la méthode POST.
+  async function post() {
+    const contact = JSON.parse(localStorage.getItem("Contact"));
+    const products = JSON.parse(localStorage.getItem("Data"));
+
+    const productsID = products.map(p => p.getId)
+    console.log(productsID)
+    const command = { contact, products: productsID }
+    console.log(command)
+
+    const postFetch = await fetch("http://localhost:3000/api/products/order", {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(command),
+    })
+    console.log(postFetch)
+    const response = await postFetch.json();
+    console.log(response);
+
+    if (response) {
+      alert(`Votre commande numéro ${response.orderId} à bien été passée.`)
+      localStorage.setItem("orderId", response.orderId)
+      window.location = `C:/Users/gpira/Desktop/Formation/P5_pirat_geoffrey/front/html/confirmation.html`
+    } else {
+      alert(`Erreur de commande`)
+    }
+  };
+});
+
+
+
+
+
 
 
